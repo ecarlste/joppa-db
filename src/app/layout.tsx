e994dc2b4aabea6daf68ada72df37418a1bba63d
Erik S. Carlsten <ecarlste@gmail.com>
2024-05-10 13:54:36 -0600

    make builds faster and add signin menu item

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Joppa DB</div>
+
+      <div>Sign In</div>
     </nav>
   );
 }
