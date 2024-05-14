7ad2156c8d1fbf073d95bbe7c38f4f5c599d8f5c
Erik S. Carlsten <ecarlste@gmail.com>
2024-05-14 16:08:41 -0600

    align table cell content with sorted headers

--- src/app/items/weapons/data_table.tsx
+++ src/app/items/weapons/data_table.tsx
@@ -70,7 +70,10 @@ export function DataTable<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell
+                    key={cell.id}
+                    className={cell.column.id === "name" ? "" : "px-8"}
+                  >
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
