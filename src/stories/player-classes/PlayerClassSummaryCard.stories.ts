9a03af2050fe4212cc192e55937650404744203e
Erik S. Carlsten <ecarlste@gmail.com>
2024-06-03 13:23:55 -0600

    add simple story for player class summary card

---
+++ src/stories/player-classes/PlayerClassSummaryCard.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { PlayerClassSummaryCard } from "~/app/classes/page";
+
+const meta = {
+  title: "Player Classes/PlayerClassSummaryCard",
+  component: PlayerClassSummaryCard,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    playerClass: { control: "object" },
+  },
+} satisfies Meta<typeof PlayerClassSummaryCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Paladin: Story = {
+  args: {
+    playerClass: {
+      id: 1,
+      name: "Paladin",
+      summary: "A holy warrior who uses divine magic to protect their allies.",
+    },
+  },
+};
