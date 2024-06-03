3248073d723a694308fbaa7ca2ed43c08c452a74
Erik S. Carlsten <ecarlste@gmail.com>
2024-06-03 09:32:40 -0600

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default async function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-items-start bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-4 px-4 py-8 ">
-        <h1 className="text-4xl font-bold">Welcome to Joppa DB Bish!</h1>
+        <h1 className="text-4xl font-bold">Welcome to Joppa DB!</h1>
       </div>
     </main>
   );
