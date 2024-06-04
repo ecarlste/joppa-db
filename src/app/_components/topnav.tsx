6ccceedad6b1d96e7c56dc785ed59f04b0c1d1d4
Erik S. Carlsten <ecarlste@gmail.com>
2024-06-04 11:33:10 -0600

    added intial nav menu component for drop downs

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "~/components/ui/navigation-menu";
+
+const playerClasses = [
+  { name: "Cleric", id: 7 },
+  { name: "Dire Lord", id: 8 },
+  { name: "Druid", id: 9 },
+];
 
 export function TopNav() {
   return (
@@ -9,12 +24,22 @@ export function TopNav() {
           <div className="text-xl font-semibold">
             <Link href="/">Joppa DB</Link>
           </div>
-          <div className="text-lg">
-            <Link href="/items/weapons">Weapons</Link>
-          </div>
-          <div className="text-lg">
-            <Link href="/classes">Classes</Link>
-          </div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Classes</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[200px] grid-cols-1 gap-3 p-4 md:w-[250px] lg:w-[300px]">
+                    {playerClasses.map((playerClass) => (
+                      <li key={playerClass.id}>
+                        <Link href={`/classes/${playerClass.id}`}>{playerClass.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
 
         <div>
